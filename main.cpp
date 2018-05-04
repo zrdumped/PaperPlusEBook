d8a263cc488844a0c1ed09637025bd02a6bfab13
Mr.Melon <nmgzhangran@163.com>
2018-05-04 18:48:35 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    cv::Mat image;
+    image = cv::imread("F:\\1.png");
+    if(image.empty())
+        return 0;
+    cv::namedWindow("image show");
+    cv::imshow("image show",image);
+
+    return a.exec();
+}
