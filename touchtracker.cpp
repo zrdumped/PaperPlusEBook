76fd3249868204217717c1065ebb745f31d9d570
vitalight <904844050@qq.com>
2018-06-22 21:21:50 +0800

    Improve tracking algorithm & Add API for use

--- touchtracker.cpp
+++ touchtracker.cpp
@@ -1,21 +1,158 @@
 ﻿#include "touchtracker.h"
 
-
 using namespace std;
-
-#define VIDEO_IN string("F:/HCI/final/resources/video/touch_test_realcut")
-#define TRACKER_TYPE 0
-#define TRACK_TWO
-#define SAVE_VIDEO
 
 // Convert to string
 #define SSTR( x ) static_cast< std::ostringstream & >( \
 ( std::ostringstream() << std::dec << x ) ).str()
 
+//#define SAVE_VIDEO
+//#define DEVIATION -7
+
+// The path for video input
+#define VIDEO_IN string("E:/touch_test_realcut")
+// The type of tracker to use (0~5)
+#define TRACKER_TYPE 0
+// define this to enable showing square and fps on ouput video
+#define TT_DEBUG
+
+// The slope of the line to detect shadow
+const float slope = 181.0f/662.0f;
+// The width of line expected to be black to return true for checkTouch
+const int expect = 12;
+static Ptr<Tracker> trackerp, trackers;
+static int offpx, offpy, offsx, offsy;
+
 TouchTracker::TouchTracker()
 {
 }
 
+/***************************************************************
+ * TouchTracker
+ * variable name: 'p' stands for pen, 's' stands for shadow
+ * *************************************************************/
+int TouchTracker::testTouch()
+{
+    // #1 - open video
+    VideoCapture video(VIDEO_IN + ".mp4");
+    if(!video.isOpened()) {
+        cout << "TouchTracker: could not locate video file "
+             << VIDEO_IN + ".mp4, check macro VIDEO_IN" << endl;
+        return 1;
+    }
+    Mat frame;
+    video.read(frame);
+
+#ifdef SAVE_VIDEO
+    VideoWriter writer(VIDEO_IN + ".avi", CV_FOURCC('D', 'I', 'V', 'X'), 18.0,
+                       Size(video.get(CV_CAP_PROP_FRAME_WIDTH),
+                       video.get(CV_CAP_PROP_FRAME_HEIGHT)));
+#endif
+
+    // #2 - setup
+    Rect2d boxp(415, 80, 54, 49);
+    initTouch(boxp, frame);
+    //initTouch(frame);
+
+    // #3 - loop for each frame
+    bool verbose = false;
+    while (video.read(frame)) {
+        int key = waitKey(1);
+        if(key == 27)
+            break;
+        else if (key == 'd')
+            verbose = !verbose;
+        else if (key == 'f') {
+            initTouch(frame);
+            continue;
+        }
+
+        if (checkTouch(frame, verbose)) {
+            putText(frame, "Touching", Point(50,80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
+        }
+        imshow("Touch Tracking", frame);
+#ifdef SAVE_VIDEO
+        writer << frame;
+#endif
+    }
+
+    destroyWindow("Touch Tracking");
+    return 0;
+}
+
+/***************************************************************
+ * API part
+ * *************************************************************/
+void TouchTracker::initTouch(Mat frame)
+{
+    Rect2d boxp;
+    boxp = selectROI(frame, false);
+    destroyWindow("ROI selector");
+    initTouch(boxp, frame);
+}
+
+void TouchTracker::initTouch(Rect2d boxp, Mat frame)
+{
+    trackerp = getTracker(TRACKER_TYPE);
+    trackerp->init(frame, boxp);
+
+    Mat imgp = Mat::Mat(frame, boxp);
+    cvtColor(imgp, imgp, CV_BGR2GRAY);
+    threshold(imgp, imgp, 0, 255, CV_THRESH_OTSU);
+    findNib(imgp, offpx, offpy);
+}
+
+bool TouchTracker::checkTouch(Mat frame, bool verbose)
+{
+#ifdef TT_DEBUG
+    double timer;
+    if (verbose)
+        timer = (double)getTickCount();
+#endif
+
+    Rect2d boxp;
+    bool okp = trackerp->update(frame, boxp);
+
+#ifdef TT_DEBUG
+    float fps;
+    if (verbose)
+        fps = getTickFrequency() / ((double)getTickCount() - timer);
+#endif
+
+    if (okp) {
+        Mat mat = Mat::Mat(frame, boxp);
+        cvtColor(mat, mat, CV_BGR2GRAY);
+        threshold(mat, mat, 0, 255, CV_THRESH_OTSU);
+        float row = mat.rows + offpy - (mat.cols + offpx) * slope;
+
+#ifdef TT_DEBUG
+        if (verbose) {
+            Point p1 = Point(boxp.x + boxp.width + offpx, boxp.y + boxp.height + offpy),
+                  p2 = Point(boxp.x, p1.y - (boxp.width + offpx) * slope),
+                  p3 = Point(boxp.x + expect, p2.y + expect * slope);
+            rectangle(frame, boxp, Scalar( 255, 0, 0 ), 2, 1 );
+            circle(frame, p1, 2, Scalar(0, 0, 255), 2);
+            putText(frame, "FPS : " + SSTR(int(fps)), Point(50,50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
+            line(frame, p2, p3, CV_RGB(0, 255, 0), 2);
+            line(frame, p3, p1, CV_RGB(255, 0, 0), 2);
+        }
+#endif
+
+        for (int col = 0; col < expect; col++) {
+            uchar *data = mat.ptr<uchar>((int)row);
+            if (data[col] != 0)
+                return false;
+            row += slope;
+        }
+        return true;
+    }
+
+    return false;
+}
+
+/********************************************
+ * Private part
+ ********************************************/
 Ptr<Tracker> TouchTracker::getTracker(int type)
 {
     // Tracking pen                         |   Tracking shadow
@@ -23,7 +160,7 @@ Ptr<Tracker> TouchTracker::getTracker(int type)
     // #1 MIL: 10fps slow but good          |   10fps and might take the shadow away with pen
     // #2 KCF: 150fps but missing sometime  |   500fps and wrong
     // #3 TLD: 20fps and not so precise     |   50fps and a little inprecise
-    // #4 MEDIANFLOW: 520fps and so great   |   fast but inprecise
+    // #4 MEDIANFLOW: 520fps and not good   |   fast but inprecise
     // #5 GOTURN : buggy and worthless
 
     // Create a tracker
@@ -52,6 +189,107 @@ Ptr<Tracker> TouchTracker::getTracker(int type)
     return tracker;
 }
 
+void TouchTracker::findNib(Mat mat, int &x, int &y)
+{
+    for (int i = mat.cols - 1; i >= 0; i--)
+    {
+        for (int j = mat.rows - 1; j >= 0; j--)
+        {
+            if (mat.at<uchar>(j, i) == 0)
+            {
+                x = i - mat.cols;
+                y = j - mat.rows;
+                return;
+            }
+        }
+    }
+}
+
+/********************************************
+ * Deprecated part
+ ********************************************/
+#ifdef FF_DEPRECATED
+void TouchTracker::initTouchOld(Mat frame)
+{
+    Rect2d boxp, boxs;
+    Mat copy = frame.clone();
+    boxp = selectROI(copy, false);
+    rectangle(copy, boxp, Scalar(255, 0, 0), 2, 1 );
+    boxs = selectROI(copy, false);
+    rectangle(copy, boxs, Scalar(255, 0, 0), 2, 1 );
+    destroyWindow("ROI selector");
+    initTouchOld(boxp, boxs, frame);
+}
+
+void TouchTracker::initTouchOld(Rect2d boxp, Mat frame)
+{
+    Rect2d boxs;
+    boxs = selectROI(frame, false);
+    destroyWindow("ROI selector");
+    initTouchOld(boxp, boxs, frame);
+}
+
+void TouchTracker::initTouchOld(Rect2d boxp, Rect2d boxs, Mat frame)
+{
+    trackerp = getTracker(TRACKER_TYPE);
+    trackers = getTracker(TRACKER_TYPE);
+
+    trackerp->init(frame, boxp);
+    trackers->init(frame, boxs);
+
+    Mat imgp = Mat::Mat(frame, boxp), imgs = Mat::Mat(frame, boxs);
+    cvtColor(imgp, imgp, CV_BGR2GRAY);
+    threshold(imgp, imgp, 0, 255, CV_THRESH_OTSU);
+    findNib(imgp, offpx, offpy);
+    //imwrite("E:/imgp.png", imgp);
+
+    cvtColor(imgs, imgs, CV_BGR2GRAY);
+    threshold(imgs, imgs, 0, 255, CV_THRESH_OTSU);
+    findNib(imgs, offsx, offsy);
+    //imwrite("E:/imgs.png", imgs);
+}
+
+bool TouchTracker::checkTouchOld(Mat frame, bool verbose)
+{
+#ifdef TT_DEBUG
+    double timer;
+    if (verbose)
+        timer = (double)getTickCount();
+#endif
+
+    Rect2d boxp, boxs;
+    bool okp = trackerp->update(frame, boxp),
+         oks = trackers->update(frame, boxs);
+
+#ifdef TT_DEBUG
+    float fps;
+    if (verbose)
+        fps = getTickFrequency() / ((double)getTickCount() - timer);
+#endif
+
+    if (okp && oks) {
+        Point p1 = Point(boxp.x + boxp.width + offpx, boxp.y + boxp.height + offpy),
+              p2 = Point(boxs.x + boxs.width + offsx, boxs.y + boxs.height + offsy);
+
+#ifdef TT_DEBUG
+        if (verbose) {
+            rectangle(frame, boxp, Scalar( 255, 0, 0 ), 2, 1 );
+            rectangle(frame, boxs, Scalar( 255, 0, 0 ), 2, 1 );
+            circle(frame, p1, 2, Scalar(0, 0, 255), 2);
+            circle(frame, p2, 2, Scalar(0, 255, 0), 2);
+            putText(frame, "FPS : " + SSTR(int(fps)), Point(50,50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
+        }
+#endif
+
+        if (p1.y - p2.y > DEVIATION)
+            return true;
+        else
+            return false;
+    }
+
+    return false;
+}
+
 int TouchTracker::example()
 {
     int type = TRACKER_TYPE;
@@ -64,7 +302,9 @@ int TouchTracker::example()
     // Read video
     VideoCapture video(VIDEO_IN + ".mp4");
     if(!video.isOpened()) {
-        cout << "Could not read video file" << endl;
+        cout << "TouchTracker: could not locate video file "
+             << VIDEO_IN + ".mp4"
+             << ", check macro VIDEO_IN" << endl;
         return 1;
     }
 #ifdef SAVE_VIDEO
@@ -76,12 +316,8 @@ int TouchTracker::example()
     Mat frame;
     bool ok = video.read(frame), ok1;
 
-    //Rect2d bbox(415, 80, 54, 49);
-    //Rect2d bbox1(399, 125, 78, 32);
-
-    Rect2d bbox0(415, 80, 54, 49);// = selectROI(frame, false);
+    Rect2d bbox0= selectROI(frame, false);
     rectangle(frame, bbox0, Scalar( 255, 0, 0 ), 2, 1 );
-    std::cout << bbox0;
     if (bbox0.area() == 0)
         return 0;
     tracker->init(frame, bbox0);
@@ -89,7 +325,6 @@ int TouchTracker::example()
 #ifdef TRACK_TWO
     Rect2d bbox1(399, 125, 78, 32);// = selectROI(frame, false);
     rectangle(frame, bbox0, Scalar( 255, 0, 0 ), 2, 1 );
-    std::cout << ", " << bbox1;
     if (bbox1.area() == 0)
         return 0;
     tracker1->init(frame, bbox1);
@@ -97,7 +332,6 @@ int TouchTracker::example()
 
     int offset1x = -8, offset1y = -3,
         offset2x = -16, offset2y = -11;
-    imshow("Tracking", frame);
     cout << endl;
 
     while(video.read(frame))
@@ -139,7 +373,7 @@ int TouchTracker::example()
 
         putText(frame, trackerType + " Tracker", Point(100,20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50),2);
         putText(frame, "FPS : " + SSTR(int(fps)), Point(100,50), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
-        imshow("Tracking", frame);
+        imshow("Touch Tracking", frame);
 #ifdef SAVE_VIDEO
         writer << frame;
 #endif
@@ -149,5 +383,7 @@ int TouchTracker::example()
             break;
         }
     }
+    destroyWindow("Touch Tracking");
     return 0;
 }
+#endif
--- touchtracker.h
+++ touchtracker.h
@@ -11,10 +11,23 @@ using namespace cv;
 class TouchTracker
 {
 public:
-    TouchTracker();
+    static int testTouch();
+    static void initTouch(Mat frame);
+    static void initTouch(Rect2d boxp, Mat frame);
+    static bool checkTouch(Mat frame, bool verbose);
+
+#ifdef FF_DEPRECATED
     static int example();
+    static void initTouchOld(Mat frame);
+    static void initTouchOld(Rect2d boxp, Mat frame);
+    static void initTouchOld(Rect2d boxp, Rect2d boxs, Mat frame);
+    static bool checkTouchOld(Mat frame, bool verbose = false);
+#endif
+
 private:
+    TouchTracker();
     static cv::Ptr<cv::Tracker> getTracker(int type);
+    static void findNib(Mat mat, int &x, int &y);
 };
 
 #endif // TOUCHTRACKER_H
